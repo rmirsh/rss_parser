c9360feaad253f820a31993bb6bf1976f2237003
Kamil Ayupov <ayupovkamil@gmail.com>
2024-03-27 21:21:29 +0300

    Create abstract classes

---
+++ abstracts.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractGetter(ABC):
+    @abstractmethod
+    def _get_response(self):
+        pass
+
+    @abstractmethod
+    def _get_data(self):
+        pass
+
+
+class AbstractLogger(ABC):
+    @abstractmethod
+    def log_to_file(self, message: str):
+        pass
