7a92f1cd76a8f7f15723903ac5b3c299220c0698
Kamil Ayupov <ayupovkamil@gmail.com>
2024-03-28 19:35:46 +0300

    Add env variables

--- config.py
+++ config.py
@@ -6,15 +6,13 @@ from pydantic import Field
 
 class Settings(BaseSettings):
 
-    RSS_URL: str = Field(env='RSS_URL')
-    TIME_SLEEP: int = Field(env='TIME_SLEEP')
-    LOG_FILE: Path = Field(env='LOG_FILE')
-    JSON_FILE: Path = Field(env='JSON_FILE')
+    RSS_URL: str = Field(env="RSS_URL")
+    TIME_SLEEP: int = Field(env="TIME_SLEEP")
+    LOG_FILE: str = Field(env="LOG_FILE")
+    JSON_FILE: str = Field(env="JSON_FILE")
+    SHOW_STATISTICS: bool = Field(env="SHOW_STATISTICS")
 
-    model_config = SettingsConfigDict(
-            env_file='.env',
-            env_file_encoding='utf-8'            
-            )
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 
 settings = Settings()
