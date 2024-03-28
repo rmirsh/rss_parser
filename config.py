17af9d5068c4a8dfe3d59491e4a6742ee75ea3b0
Kamil Ayupov <ayupovkamil@gmail.com>
2024-03-28 21:35:15 +0300

    Rename vars

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from pydantic import Field
 class Settings(BaseSettings):
 
     RSS_URL: str = Field(env="RSS_URL")
-    TIME_SLEEP: int = Field(env="TIME_SLEEP")
+    INTERVAL: int = Field(env="INTERVAL")
     LOG_FILE: str = Field(env="LOG_FILE")
     JSON_FILE: str = Field(env="JSON_FILE")
     SHOW_STATISTICS: bool = Field(env="SHOW_STATISTICS")
