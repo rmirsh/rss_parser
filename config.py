0f43c92c15f25805b17a201ecbbbbfa93b4109d3
Kamil Ayupov <ayupovkamil@gmail.com>
2024-03-30 12:54:42 +0300

    Change str validation to Path

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Settings(BaseSettings):
 
     RSS_URL: str = Field(env="RSS_URL")
     INTERVAL: int = Field(env="INTERVAL")
-    LOG_FILE: str = Field(env="LOG_FILE")
-    JSON_FILE: str = Field(env="JSON_FILE")
+    LOG_FILE: Path = Field(env="LOG_FILE")
+    JSON_FILE: Path = Field(env="JSON_FILE")
     SHOW_STATISTICS: bool = Field(env="SHOW_STATISTICS")
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
